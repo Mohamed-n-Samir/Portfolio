c580c36aa69ecec0553b3ed8008d959ccfd5e909
Mohamed Samir <mohamed.n.work12@gmail.com>
2026-06-26 21:48:30 +0300

    adding basename to react router to match the config

--- src/main.tsx
+++ src/main.tsx
@@ -9,23 +9,28 @@ import ErrorPage from "./components/ErrorComponent";
 
 import "./index.css";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "*",
+          element: <NotFound />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "*",
-        element: <NotFound />,
-      },
-    ],
+    basename: import.meta.env.BASE_URL,
   },
-]);
+);
 
 const root = document.getElementById("root") as HTMLElement;
 
