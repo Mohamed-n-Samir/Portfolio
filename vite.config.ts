7c9f6570027acb9c3732a1005c121ba6c7cb2636
Mohamed Samir <mohamed.n.work12@gmail.com>
2026-06-30 17:15:53 +0300

    trying new github-pages trick editing base

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/Portfolio/",
+  base: process.env.VITE_BASE_PATH || '/',
   server: {
     open: true,
     port: 3001,
